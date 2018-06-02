c9a02d62d1a3c798fe92d10a5722729792963548
vog3l <mail@vog3l.de>
2018-06-02 17:59:13 +0200

    testing

--- lib/ldap_ecto/adapter.ex
+++ lib/ldap_ecto/adapter.ex
@@ -291,11 +291,15 @@ defmodule Ldap.Ecto.Adapter do
   @impl true
   def dumpers(_, nil), do: {:ok, nil}
   def dumpers({:in, _type}, {:in, _}), do: [&Helper.dump_in/1]
-  def dumpers(:string, _type), do: [&Helper.dump_string/1]
+  def dumpers(:string, _type) do
+    asd = break
+
+    [&Helper.dump_string/1]
+  end
   def dumpers({:array, :string}, _type) do
-    &(Enum.each(&1, fn(x) ->
-      Helper.dump_string(x)
-    end))
+    asd = break
+
+    [&Helper.dump_array/1]
   end
   def dumpers(:datetime, _type), do: [&Helper.dump_date/1]
   def dumpers(Ecto.DateTime, _type), do: [&Helper.dump_date/1]
