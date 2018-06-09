d5137d56454c23f0a5e3d1f30f89a8cca0618366
vog3l <mail@vog3l.de>
2018-06-09 19:42:28 +0200

    bug fix

--- lib/ldap_ecto/schema.ex
+++ lib/ldap_ecto/schema.ex
@@ -2,8 +2,8 @@ defmodule Ldap.Ecto.Schema do
   defmacro __using__(_) do
     quote do
       use Ecto.Schema
-      @primary_key {:cn, :binary_id, autogenerate: true}
-      @foreign_key_type :binary_id
+      @primary_key {:dn, :string, autogenerate: false}
+      @foreign_key_type :string
     end
   end
 end
